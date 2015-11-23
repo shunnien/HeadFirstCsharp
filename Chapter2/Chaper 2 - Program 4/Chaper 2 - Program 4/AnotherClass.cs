2095f8c33011eb0786ba365edadffb30b5bc5068
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2015-11-23 14:34:28 +0800

    chaper 2 win form Sample

---
+++ Chapter2/Chaper 2 - Program 4/Chaper 2 - Program 4/AnotherClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaper_2___Program_4
+{
+    class AnotherClass
+    {
+        public static void Main()
+        {
+            MessageBox.Show("Wow!!");
+        }
+    }
+}
