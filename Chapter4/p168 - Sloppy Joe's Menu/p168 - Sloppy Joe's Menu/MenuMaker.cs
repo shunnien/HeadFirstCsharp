d72e867b3aa58dc9101943edf988f052bacd544b
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2016-01-18 22:57:47 +0800

    add GetMenuItem() to MenuMaker Class

--- Chapter4/p168 - Sloppy Joe's Menu/p168 - Sloppy Joe's Menu/MenuMaker.cs
+++ Chapter4/p168 - Sloppy Joe's Menu/p168 - Sloppy Joe's Menu/MenuMaker.cs
@@ -8,8 +8,15 @@ namespace p168___Sloppy_Joe_s_Menu {
     class MenuMaker {
         public Random Randomizer;
 
-        public string[] Meats = {"Roast beef","Salami","Turkey","Ham","Pastrami" };
-        public string[] Condiments = {"yellow mustard","brown mustard", "honey mustard","mayo","relish","french dressing" };
-        public string[] Breads = {"rye","white","wheat","pumpernickel","italian bread","a roll" };
+        string[] Meats = {"Roast beef","Salami","Turkey","Ham","Pastrami" };
+        string[] Condiments = {"yellow mustard","brown mustard", "honey mustard","mayo","relish","french dressing" };
+        string[] Breads = {"rye","white","wheat","pumpernickel","italian bread","a roll" };
+
+        public string GetMenuItem() {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat + " with " + randomCondiment + " on " + randomBread;
+        }
     }
 }
