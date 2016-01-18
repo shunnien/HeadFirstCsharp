4fbe1745326fe675f2b933ccb2d0925faf38ca71
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2016-01-18 22:53:14 +0800

    p.168 Sloppy Joe

---
+++ Chapter4/p168 - Sloppy Joe's Menu/p168 - Sloppy Joe's Menu/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p168___Sloppy_Joe_s_Menu {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Chapter4/p168 - Sloppy Joe's Menu/p168 - Sloppy Joe's Menu/MenuMaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p168___Sloppy_Joe_s_Menu {
+    class MenuMaker {
+        public Random Randomizer;
+
+        public string[] Meats = {"Roast beef","Salami","Turkey","Ham","Pastrami" };
+        public string[] Condiments = {"yellow mustard","brown mustard", "honey mustard","mayo","relish","french dressing" };
+        public string[] Breads = {"rye","white","wheat","pumpernickel","italian bread","a roll" };
+    }
+}
