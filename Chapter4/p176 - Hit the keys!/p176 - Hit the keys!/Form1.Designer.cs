ad7d4bc7ca4c9e9f10c4813a62cb4e9ca1e985b4
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2016-02-16 00:43:47 +0800

    Add Stats.cs and set timer1_Tick

--- Chapter4/p176 - Hit the keys!/p176 - Hit the keys!/Form1.Designer.cs
+++ Chapter4/p176 - Hit the keys!/p176 - Hit the keys!/Form1.Designer.cs
@@ -51,6 +51,7 @@
             // timer1
             // 
             this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // statusStrip1
             // 
--- Chapter4/p176 - Hit the keys!/p176 - Hit the keys!/Form1.cs
+++ Chapter4/p176 - Hit the keys!/p176 - Hit the keys!/Form1.cs
@@ -10,8 +10,20 @@ using System.Windows.Forms;
 
 namespace p176___Hit_the_keys_ {
     public partial class Form1 : Form {
+        Random random = new Random();
+        Stats stats = new Stats();
         public Form1() {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            // Add a random key to the ListBox
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count > 7) {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game over");
+                timer1.Stop();
+            }
         }
     }
 }
