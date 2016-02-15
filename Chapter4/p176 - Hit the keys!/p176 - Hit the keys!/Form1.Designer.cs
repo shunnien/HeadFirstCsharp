2c0ae42034c70c2e50506759d7a635759cd5443b
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2016-02-16 00:34:07 +0800

    Set statusStrip1 property SizingGrip = False

--- Chapter4/p176 - Hit the keys!/p176 - Hit the keys!/Form1.Designer.cs
+++ Chapter4/p176 - Hit the keys!/p176 - Hit the keys!/Form1.Designer.cs
@@ -60,6 +60,7 @@
             this.statusStrip1.Location = new System.Drawing.Point(0, 106);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(856, 25);
+            this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
