aa31f38f971f26661e410c54f6d01a4d925fec63
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2016-02-16 00:31:49 +0800

    Hit the keys

---
+++ Chapter4/p176 - Hit the keys!/p176 - Hit the keys!/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p176___Hit_the_keys_ {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
